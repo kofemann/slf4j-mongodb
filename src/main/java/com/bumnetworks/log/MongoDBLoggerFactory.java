ae2e2c5a2343af01024dfcb491b2ae7d95178633
Max Afonov <max@bumnetworks.com>
2009-10-10 22:33:02 -0400

    formatting

--- src/main/java/com/bumnetworks/log/MongoDBLoggerFactory.java
+++ src/main/java/com/bumnetworks/log/MongoDBLoggerFactory.java
@@ -8,24 +8,24 @@ import org.slf4j.ILoggerFactory;
 
 public class MongoDBLoggerFactory implements ILoggerFactory {
 
-  final static MongoDBLoggerFactory INSTANCE = new MongoDBLoggerFactory();
+    final static MongoDBLoggerFactory INSTANCE = new MongoDBLoggerFactory();
 
-  Map loggerMap;
+    Map loggerMap;
 
-  public MongoDBLoggerFactory() {
-    loggerMap = new HashMap();
-  }
-
-  public Logger getLogger(String name) {
-    Logger slogger = null;
-    // protect against concurrent access of the loggerMap
-    synchronized (this) {
-      slogger = (Logger) loggerMap.get(name);
-      if (slogger == null) {
-        slogger = new MongoDBLogger(name);
-        loggerMap.put(name, slogger);
-      }
+    public MongoDBLoggerFactory() {
+        loggerMap = new HashMap();
     }
-    return slogger;
-  }
+
+    public Logger getLogger(String name) {
+        Logger slogger = null;
+        // protect against concurrent access of the loggerMap
+        synchronized (this) {
+            slogger = (Logger) loggerMap.get(name);
+            if (slogger == null) {
+                slogger = new MongoDBLogger(name);
+                loggerMap.put(name, slogger);
+            }
+        }
+        return slogger;
+    }
 }
